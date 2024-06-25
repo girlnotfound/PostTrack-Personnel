af04e62c3779ab3a7004ccdd9d34b1c5b6c59425
girlnotfound <150276019+girlnotfound@users.noreply.github.com>
2024-06-25 11:04:54 -0700

    Added main menu function for viewing and managing departments, roles and employees

--- index.js
+++ index.js
@@ -12,3 +12,51 @@ const {
   addEmployee,
   updateEmployeeRole,
 } = require('./query');
+
+// main menu function
+const mainMenu = async () => {
+  const answer = await inquirer.prompt({
+    type: 'list',
+    name: 'action',
+    message: 'What would you like to do?',
+    choices: [
+      'View all departments',
+      'View all roles',
+      'View all employees',
+      'Add a department',
+      'Add a role',
+      'Add an employee',
+      'Update an employee role',
+      'Exit'
+    ],
+  });
+  switch (answer.action) {
+    case 'View all departments':
+      const departments = await viewDepartments();
+      console.table(departments);
+      break;
+    case 'View all roles':
+      const roles = await viewRoles();
+      console.table(roles);
+      break;
+    case 'View all employees':
+      const employees = await viewEmployees();
+      console.table(employees);
+      break;
+    case 'Add a department':
+      await promptAddDepartment();
+      break;
+    case 'Add a role':
+      await promptAddRole();
+      break;
+    case 'Add an employee':
+      await promptAddEmployee();
+      break;
+    case 'Update an employee role':
+      await promptUpdateEmployeeRole();
+      break;
+    case 'Exit':
+      process.exit();
+  }
+  mainMenu(); // loop back to main menu after an action is completed
+};
